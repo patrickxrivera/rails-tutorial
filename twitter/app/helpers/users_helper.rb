b76fc4bfc50be059c5d330b7e193f45febd8327b
Patrick Rivera <patrick.rivera@instacart.com>
2018-11-22 16:07:30 -0800

    Added signup

--- twitter/app/helpers/users_helper.rb
+++ twitter/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    GRAVATAR_ROOT_URL = "https://secure.gravatar.com/avatar"
+
+    def gravatar_for(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "#{GRAVATAR_ROOT_URL}/#{gravatar_id}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
